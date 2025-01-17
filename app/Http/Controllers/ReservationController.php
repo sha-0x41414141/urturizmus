ab0ecbea6f4d3916527d2a0b23c2e6b69feb5581
sha-0x41414141 <87914329+sha-0x41414141@users.noreply.github.com>
2025-01-17 11:28:36 +0100

    reserv

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class ReservationController extends Controller
 {
     public function index()
     {
-        $reservations = Auth::user()::with('destinations')->get();
+        $userlogged = Auth::user();
+        $reservations = User::where('id', '=', $userlogged->id)->first()->destinations()->get();
         return view('reservations.index', compact('reservations'));
     }
 }
